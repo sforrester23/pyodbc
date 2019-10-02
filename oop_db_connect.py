4c5189a6af96f65784b8304e8dce8df5a726d707
Sam Forrester <SForrester@spartaglobal.com>
2019-10-02 12:21:12 +0100

    Made many more methods for doing various things with our SQL queries, in our db connect class

--- oop_db_connect.py
+++ oop_db_connect.py
@@ -1,5 +1,9 @@
 import pyodbc
 
+# We should consider the concept of "Strong Params": never EVER trust user inputs
+# Avoid SQL injections
+# Filter for SQL injections
+
 class ConnectMsSQL():
 
     def __init__(self, server, database, username, password):
@@ -10,6 +14,54 @@ class ConnectMsSQL():
         self.connect_db = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+self.server+';DATABASE='+self.database+';UID='+self.username+';PWD='+self.password)
         self.cursor = self.connect_db.cursor()
 
+    def __filter_query(self, sqlquery):
+        return self.cursor.execute(sqlquery)
+        # Doing some filtering for bad queries
+
+    def __sql_query(self, sqlquery): # we encapsulate this and then only call it in other methods
+        return self.__filter_query(sqlquery)
+
+    def sql_query_fetchone(self, query):
+        return self.__filter_query(query).fetchone()
+
+    def sql_query_fetchall(self, query):
+        return self.__filter_query().fetchall()
+
+    def print_all_records(self, table):
+        query_rows = self.__filter_query('SELECT * FROM {}'.format(table))
+        while True:
+            record = query_rows.fetchone()
+            if record is None:
+                break
+            print(record)
+
+    def print_average_unit_price(self):
+        query_rows = self.__filter_query('SELECT AVG(UnitPrice) FROM Products')
+        while True:
+            record = query_rows.fetchone()
+            if record is None:
+                break
+            print(record)
+
+    def return_average_unit_price_products(self):
+        # Query
+        query = self.__filter_query('SELECT * FROM Products')
+        # Get all the unit prices
+        prices = []
+        while True:
+            # get individual prices
+            # append to prices list
+            record = query.fetchone()
+            if record is None:
+                break
+            prices.append(record.UnitPrice)
+        # sum and divide by length of rows
+        return (sum(prices)/len(prices))
 
 
+    def total_sales_for_company(self):
+        query_rows = self.__filter_query("SELECT CompanyName, SUM(ROUND(UnitPrice*Quantity*(1-Discount),2)) FROM [Order Details] OD "
+                                         "JOIN Orders O ON OD.OrderID=O.OrderID"
+                                         "JOIN Customers C ON C.CustomerID=O.CustomerID"
+                                         "GROUP BY CompanyName")
 
--- oop_nw_db_run.py
+++ oop_nw_db_run.py
@@ -9,4 +9,11 @@ db_nw = ConnectMsSQL(server, database, username, password)
 print(db_nw)
 print(db_nw.connect_db)
 
-print(db_nw.cursor.execute('SELECT * FROM Products').fetchone())
+# print(db_nw.cursor.execute('SELECT * FROM Products').fetchone())
+# We can do this more succinctly:
+
+print(db_nw.sql_query_fetchone("SELECT * FROM Products"))
+
+# db_nw.print_all_records('Products')
+
+print(db_nw.return_average_unit_price_products())
